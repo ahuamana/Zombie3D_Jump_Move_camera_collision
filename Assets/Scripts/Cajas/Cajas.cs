8e33c582670e5875243f97a6ee0a472a222100a3
Alikhan <73962071@continental.edu.pe>
2021-06-04 14:21:43 -0500

    added script to move platforms

---
+++ Assets/Scripts/Cajas/Cajas.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cajas : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "CajaRed")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlataformasMoviles/PlatformMovil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovil : MonoBehaviour
+{
+
+    public Rigidbody platformRB;
+    public Transform[] platformPositions;
+    public float platformSpeed;
+
+    private int actualPosition = 0;
+    private int nextPosition = 1;
+
+    
+    void Update()
+    {
+        MovePlatform();
+    }
+
+    private void MovePlatform()
+    {
+        //Move current position to a target
+        platformRB.MovePosition(Vector3.MoveTowards(platformRB.position, platformPositions[nextPosition].position, platformSpeed * Time.deltaTime));
+
+        //Calcula la distancia entre dos vectores
+        if (Vector3.Distance(platformRB.position, platformPositions[nextPosition].position) <= 0)
+        {
+            actualPosition = nextPosition;
+            nextPosition++;
+
+            //
+            if (nextPosition > platformPositions.Length - 1)
+            {
+                nextPosition = 0;
+            }
+
+        }
+
+    }
+}
