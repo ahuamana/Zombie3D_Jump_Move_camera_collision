c0ab8e43aad1bb603b4848e105adc35c3e88c726
Alikhan <73962071@continental.edu.pe>
2021-06-03 22:31:09 -0500

    added script to destroy item monedas when it collision

---
+++ Assets/Scripts/Monedas/MonedasCount.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonedasCount : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Monedas")
+        {
+            Debug.Log("Colisono");
+
+            Destroy(collision.gameObject, 0.1f);
+        }
+    }
+}
